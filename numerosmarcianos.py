676cce2af4d427de9ddf68476367bf432d6f83f5
Poxxus <PedroLinoBr2015@hotmail.com>
2022-12-15 19:53:29 -0300

    Código que faz uma pirâmide de tamanho configurável.

--- numerosmarcianos.py
+++ numerosmarcianos.py
@@ -1,12 +1,15 @@
-from sys import stdin
-i = str
-substitutos = [("8", "4"), ("9", "5"), ("7", "6"), ("2", "9"), ("5", "1"), ("6", "2"), ("4", "3"), ("3", "7"), ("1", "8")]
-for i in stdin:
-    if i == "0":
-        break
-    else:
-        for num, marc in substitutos:
-            if num in i:
-                i = i.replace(num, marc)
-                print(i)
-                break
+substituitos = [0, 8, 9, 7, 3, 1, 2, 6, 4, 5]
+
+user_input = str(input())
+
+while user_input != "0":
+    list_of_ints = [int(x) for x in user_input]
+
+    result_list = []
+
+    for elemento_lista_entrada in list_of_ints:
+        result_list.append(substituitos[elemento_lista_entrada])
+
+    print(''.join(map(str, result_list)))
+
+    user_input = str(input())
