7380232719b1fff719dc877238cca01dc3928228
poxxus <116926876+poxxus@users.noreply.github.com>
2023-06-09 14:57:58 -0300

    Update cores.py

--- cores.py
+++ cores.py
@@ -1,26 +1,27 @@
 def cores():
-    r = bool(int(input()))
-    g = bool(int(input()))
-    b = bool(int(input()))
+    r = bool(int(input()))      # vermelho
+    g = bool(int(input()))      # verde
+    b = bool(int(input()))      # azul
     if b:
         if r:
             if g:
-                print("7")
+                print("7")      # branco
             else:
-                print("2")
+                print("2")      # roxo
         elif g:
-            print("6")
+            print("6")      # ciano
         else:
-            print("1")
+            print("1")      # azul
     elif r:
         if g:
-            print("4")
+            print("4")      # amarelo
         else:
-            print("3")
+            print("3")      #vermelho
     elif g:
-        print("5")
+        print("5")      # verde
     else:
-        print("8")
+        print("8")      # preto
 
 
 cores()
+
