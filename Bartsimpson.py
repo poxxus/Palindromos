74c36f7982f754c4937b6a72d862bd1314740b93
poxxus <pedrolinobr2015@hotmail.com>
2023-06-18 11:21:34 -0300

    Código que imita o Bart na abertura dos Simpsons.

---
+++ Bartsimpson.py
@@ -0,0 +1,10 @@
+def bartsimpson():
+    qtidade = int(input())
+    frase = str(input())
+    contador = 0
+    while contador < qtidade:
+        print(f"{frase}")
+        contador += 1
+
+
+bartsimpson()
