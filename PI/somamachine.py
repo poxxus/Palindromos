a4bf109ad48ce9924689f5a5df3a01db578ecf65
poxxus <pedrolinobr2015@hotmail.com>
2023-06-18 11:39:45 -0300

    Organizei as pastas.

---
+++ PI/somamachine.py
@@ -0,0 +1,12 @@
+def somamachine():
+    qtidade = int(input())
+    acc = 0
+    contador = 1
+    while contador <= qtidade:
+        numero = float(input())
+        acc += numero
+        contador += 1
+    print(f"Total: {acc}")
+
+
+somamachine()
