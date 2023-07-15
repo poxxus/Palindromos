dae67f32dc6c1a2147537c139824e27d95f47d9b
poxxus <pedrolinobr2015@hotmail.com>
2023-07-15 15:43:21 -0300

    Mudança simples

--- PI/encontra_par_soma.py
+++ PI/encontra_par_soma.py
@@ -3,8 +3,8 @@ def encontrar_par():
     soma = int(input())
     acc = 0
     for i in range(0, len(lista)):
-        for j in range(i, len(lista)):
-            if (int(lista[i]) + int(lista[j])) == soma and i != j:
+        for j in range(i + 1, len(lista)):
+            if (int(lista[i]) + int(lista[j])) == soma:
                 print(f"X[{i}] + X[{j}] = {soma}")
                 acc += 1
     if acc == 0:
