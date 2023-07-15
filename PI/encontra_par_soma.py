132c909103c9807a815467d7c19f8afcc4d39b6f
poxxus <pedrolinobr2015@hotmail.com>
2023-07-15 15:10:29 -0300

    Programa que encontra um par que soma tanto em uma lista.

---
+++ PI/encontra_par_soma.py
@@ -0,0 +1,14 @@
+def encontrar_par():
+    lista = input().split(" ")
+    soma = int(input())
+    acc = 0
+    for i in range(0, len(lista)):
+        for j in range(i, len(lista)):
+            if (int(lista[i]) + int(lista[j])) == soma and i != j:
+                print(f"X[{i}] + X[{j}] = {soma}")
+                acc += 1
+    if acc == 0:
+        print(f"NENHUM PAR SOMA {soma}")
+
+
+encontrar_par()
